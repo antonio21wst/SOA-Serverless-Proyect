953a9cf49a5b0075b61f62188ee4ba0ed47bfbec
antonio21wst <joseantoniocj33@gmail.com>
2025-05-18 23:07:16 -0600

    Version-1.1

--- crud_service/nosql_crud_service.py
+++
@@ -1,7 +0,0 @@
-# crud_service/nosql_crud_service.py
-from base_service import CRUDServiceBase
-from handlers.nosql_handler import handle_nosql_message
-
-if __name__ == "__main__":
-    service = CRUDServiceBase(queue_name="nosql_queue", handler_function=handle_nosql_message)
-    service.start()
--- crud_service/sql_crud_service.py
+++
@@ -1,7 +0,0 @@
-# crud_service/sql_crud_service.py
-from base_service import CRUDServiceBase
-from handlers.sql_handler import handle_sql_message
-
-if __name__ == "__main__":
-    service = CRUDServiceBase(queue_name="sql_queue", handler_function=handle_sql_message)
-    service.start()
